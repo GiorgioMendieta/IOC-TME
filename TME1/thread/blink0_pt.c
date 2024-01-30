2b71aa3aed17e10f60a40832e8e39d1755e676d5
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2024-01-30 22:00:23 +0100

    Change thread parameter passing - Rename variables - Add thread creation error message

--- TME1/thread/blink0_pt.c
+++ TME1/thread/blink0_pt.c
@@ -20,9 +20,9 @@
 #define BCM2835_PERIPH_BASE 0x20000000
 #define BCM2835_GPIO_BASE (BCM2835_PERIPH_BASE + 0x200000)
 
-#define GPIO_LED0 4  // +5V
-#define GPIO_LED1 17 // +3.3V
-#define GPIO_BP 18   // GPIO 24
+#define GPIO_LED0 4
+#define GPIO_LED1 17
+#define GPIO_PUSH_BUTTON 18
 
 #define GPIO_FSEL_INPUT 0
 #define GPIO_FSEL_OUTPUT 1
@@ -45,14 +45,18 @@ struct gpio_s
     uint32_t test[1];
 };
 
+// Structure to send parameters to threads
 struct blink_params
 {
     int led;
-    int period;
+    int half_period;
 };
 
+// Note: Volatile is used here to prevent optimizations from changing where the variable is stored, we always want
+// to store it in memory and not in registers
 volatile struct gpio_s *gpio_regs_virt;
 
+// GPIO Function Select
 static void
 gpio_fsel(uint32_t pin, uint32_t fun)
 {
@@ -62,15 +66,23 @@ gpio_fsel(uint32_t pin, uint32_t fun)
     gpio_regs_virt->gpfsel[reg] = (gpio_regs_virt->gpfsel[reg] & ~mask) | ((fun << bit) & mask);
 }
 
+// GPIO Write
 static void
 gpio_write(uint32_t pin, uint32_t val)
 {
     uint32_t reg = pin / 32;
     uint32_t bit = pin % 32;
+
     if (val == 1)
+    {
+        // Set GPIO 1
         gpio_regs_virt->gpset[reg] = (1 << bit);
+    }
     else
+    {
+        // Clear GPIO 0
         gpio_regs_virt->gpclr[reg] = (1 << bit);
+    }
 }
 
 //------------------------------------------------------------------------------
@@ -94,8 +106,7 @@ gpio_mmap(void **ptr)
         return -1;
     }
 
-    mmap_result = mmap(
-        NULL, RPI_BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, mmap_fd, BCM2835_GPIO_BASE);
+    mmap_result = mmap(NULL, RPI_BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, mmap_fd, BCM2835_GPIO_BASE);
 
     if (mmap_result == MAP_FAILED)
     {
@@ -125,38 +136,42 @@ void delay(unsigned int milisec)
     nanosleep(&ts, &dummy);
 }
 
-void blink(int led, int half_period)
+void *blink(void *threadarg)
 {
+    // Each thread receives a unique instance of the structure
+    struct blink_params *args;
+    args = (struct blink_params *)threadarg;
+    int led = args->led;
+    int half_period = args->half_period;
+
     // Setup GPIO to output
     gpio_fsel(led, GPIO_FSEL_OUTPUT);
 
-    uint32_t val = 0;
+    // Begin alternating the value
+    uint32_t is_on = 0;
     while (1)
     {
-        gpio_write(led, val);
+        gpio_write(led, is_on);
         delay(half_period);
-        val = 1 - val;
+        is_on = 1 - is_on; // is_on = not is_on
     }
 }
 
 int main(int argc, char **argv)
 {
     // Get args
-    // ---------------------------------------------
-
     int period, half_period;
 
-    period = 1000; /* default = 1Hz */
+    period = 1000; /* default = 1Hz (1000 ms) */
     if (argc > 1)
     {
         period = atoi(argv[1]);
     }
+    // Half of the time on, the other half off
     half_period = period / 2;
     uint32_t volatile *gpio_base = 0;
 
-    // map GPIO registers
-    // ---------------------------------------------
-
+    // Map GPIO registers
     if (gpio_mmap((void **)&gpio_regs_virt) < 0)
     {
         printf("-- error: cannot setup mapped GPIO.\n");
@@ -164,36 +179,37 @@ int main(int argc, char **argv)
     }
 
     // Setup GPIO of LED0 to output
-    // ---------------------------------------------
-
     gpio_fsel(GPIO_LED0, GPIO_FSEL_OUTPUT);
 
-    // Blink led at frequency of 1Hz
-    // ---------------------------------------------
-
-    uint32_t val = 0;
-
+    // Blink led at default frequency of 1Hz
     printf("-- info: start blinking.\n");
 
     // Create thread to blink another led
+    // (https://hpc-tutorials.llnl.gov/posix/passing_args/)
 
-    // Parametres pour le thread
+    // Parameters for the thread
+    // Create a local structure and set the members' values
     struct blink_params blink1_args;
     blink1_args.led = GPIO_LED1;
-    blink1_args.period = half_period;
+    blink1_args.half_period = half_period;
 
+    // Create the thread and pass the structure containing the values as argument
     pthread_t thread;
-    int ret = pthread_create(&thread, NULL, blink, &blink1_args);
+    int ret = pthread_create(&thread, NULL, blink, (void *)&blink1_args);
     if (!ret)
     {
+        // Join the thread in case of error
+        printf("-- error: cannot create thread.\n");
         pthread_join(thread, NULL);
     }
 
+    // Begin alternating the value
+    uint32_t is_on = 0;
     while (1)
     {
-        gpio_write(GPIO_LED0, val);
+        gpio_write(GPIO_LED0, is_on);
         delay(half_period);
-        val = 1 - val;
+        is_on = 1 - is_on; // is_on = not is_on
     }
 
     pthread_join(thread, NULL);
