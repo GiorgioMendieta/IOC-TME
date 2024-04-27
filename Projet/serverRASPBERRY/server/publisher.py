1de5ce6e3b33354d7365e1585ef562d2c7e93c2e
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2024-04-27 19:20:50 +0200

    Update RPI MQTT publisher script

--- Projet/serverRASPBERRY/server/publisher.py
+++ Projet/serverRASPBERRY/server/publisher.py
@@ -1,27 +1,35 @@
 import paho.mqtt.client as mqtt
 
+MQTT_BROKER = "192.168.1.95"
+CLIENT_ID = ""
+
 # Création de la fifo vers le server
 s2fName = '/tmp/s2f_TM'
 s2f = open(s2fName,'w+')
-#res = s2f.readline()
 
-# Publishment
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+	print("Connected to ", client._host, "port: ", client._port)
+	print("Flags: ", flags, "returned code: ", rc)
+	client.subscribe("/inTopic", qos=0)
 
-def on_connect(client, userdata, flags, rc, truc): # Callback connexion broker
-    print("Connecté " + str(rc))
-    #client.subscribe("hello")
+# The callback for when a message is received from the server.
+def on_message(client, userdata, msg):
+	print("sisub: msg received with topic: {} and payload: {}".format(msg.topic, str(msg.payload)))
 
-#def on_message(client, userdata, msg): # Callback subscriber
-#    print("Message reçu :", msg.topic, str(msg.payload))
+# Version 2 of the callback API since the version 1 is deprecated
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+#client = mqtt.Client(client_id=CLIENT_ID, clean_session=True, userdata=None, protocol=mqtt.MQTTv311, transport="tcp")
 
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2) # Création d'une instance client
-#client = mqtt.Client()
-client.on_connect = on_connect # Assignation des callbacks
-#client.on_message = on_message
-client.connect("192.168.1.95", 1883, 60) # Connexion au broker
+# Assignation des callbacks
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.username_pw_set(None, password=None)
+client.connect(MQTT_BROKER, port=1883, keepalive=60) # Connexion au broker
 
 while(1):
 	res = s2f.readline()
 	if(res):
-		client.publish("topic", res) # Publication d'un message
+		client.publish("/outTopic", res) # Publication d'un message
 #client.loop_forever() # pour attendre les messages
