eee65e66748f2cc075a22fa8e7c120962f014a24
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2024-04-28 16:16:08 +0200

    Implement tasks with timers

--- Projet/Preuve_de_concept/MQTT/mqtt_esp32/mqtt_esp32.ino
+++ Projet/Preuve_de_concept/MQTT/mqtt_esp32/mqtt_esp32.ino
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
-// #include "WiFiCredentials.h" // File containing WiFi credentials
 
 // --------------------------------------------------------------------------------------------------------------------
+// Definitions
 // WIFI credentials
 #define WIFI_SSID "Coloc_Apt64"
 #define WIFI_PASSWORD "Carbonara2023*"
@@ -11,6 +11,15 @@
 // #define MQTT_BROKER "192.168.1.133"
 #define MQTT_BROKER "test.mosquitto.org"
 #define MQTT_PORT 1883
+// Pins
+#define PHOTOR_PIN 36
+// Timer
+#define MAX_WAIT_FOR_TIMER 5
+#define TIMER0 0
+#define TIMER1 1
+#define TIMER2 2
+#define TIMER3 3
+#define TIMER4 4
 // WiFi parameters
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASSWORD;
@@ -26,10 +35,102 @@ unsigned long lastPublishTime = 0;
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
 
+// --------------------------------------------------------------------------------------------------------------------
 // Function prototypes
 void setup_wifi();
 void connect_mqtt();
 void callback(char *topic, byte *payload, unsigned int length);
+unsigned int waitFor(int timer, unsigned long period);
+void setup_Lum(struct Photoresistance_s *ctx, int timer, unsigned long period);
+void loop_Lum(struct Photoresistance_s *ctx);
+
+// --------------------------------------------------------------------------------------------------------------------
+// unsigned int waitFor(timer, period)
+// Timer pour taches périodiques
+// configuration :
+//  - MAX_WAIT_FOR_TIMER : nombre maximum de timers utilisés
+// arguments :
+//  - timer  : numéro de timer entre 0 et MAX_WAIT_FOR_TIMER-1
+//  - period : période souhaitée
+// retour :
+//  - nombre de périodes écoulées depuis le dernier appel
+// --------------------------------------------------------------------------------------------------------------------
+unsigned int waitFor(int timer, unsigned long period)
+{
+  static unsigned long waitForTimer[MAX_WAIT_FOR_TIMER]; // il y a autant de timers que de tâches périodiques
+  unsigned long newTime = micros() / period;             // numéro de la période modulo 2^32
+  int delta = newTime - waitForTimer[timer];             // delta entre la période courante et celle enregistrée
+  if (delta < 0)
+    delta = 1 + newTime; // en cas de dépassement du nombre de périodes possibles sur 2^32
+  if (delta)
+    waitForTimer[timer] = newTime; // enregistrement du nouveau numéro de période
+  return delta;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// Photo resistance task definition
+
+struct Photoresistance_s
+{
+  int timer;
+  unsigned long period;
+  int val;
+  int pin;
+};
+
+void setup_Photo(struct Photoresistance_s *ctx, int timer, unsigned long period)
+{
+  ctx->timer = timer;
+  ctx->period = period;
+  ctx->val = 0;
+  ctx->pin = PHOTOR_PIN;
+}
+
+void loop_Photo(struct Photoresistance_s *ctx)
+{
+  // Wait for the period to elapse
+  if (!waitFor(ctx->timer, ctx->period))
+    return;
+
+  ctx->val = map(analogRead(ctx->pin), 4095, 0, 0, 100); // Map the value to a 0-100 range
+  snprintf(msg, MSG_BUFFER_SIZE, "Photoresistance value: %ld", ctx->val);
+  Serial.print("Message published: ");
+  Serial.println(msg);
+  client.publish("/esp32/photoresistance", msg);
+}
+
+// Message publication task
+
+struct Publish_s
+{
+  int timer;
+  unsigned long period;
+  char topic[20];
+  char msg[40];
+};
+
+void setup_Publish(struct Publish_s *ctx, int timer, unsigned long period, const char *topic)
+{
+  ctx->timer = timer;
+  ctx->period = period;
+  strcpy(ctx->topic, topic);
+  strcpy(ctx->msg, "");
+}
+
+void loop_Publish(struct Publish_s *ctx, const char *msg)
+{
+  // Wait for the period to elapse
+  if (!waitFor(ctx->timer, ctx->period))
+    return;
+
+  strcpy(ctx->msg, msg);
+  client.publish(ctx->topic, ctx->msg);
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// Task declarations
+struct Photoresistance_s Photoresistance;
+struct Publish_s Publish;
 
 // Connect to WiFi
 void setup_wifi()
@@ -54,7 +155,7 @@ void setup_wifi()
     count++;
     if (count >= max_retries)
     {
-      Serial.println("Connection failed");
+      Serial.println("Connection failed, restarting...");
       ESP.restart(); // Reset the ESP after 60 seconds
       return;
     }
@@ -70,11 +171,12 @@ void setup_wifi()
 // TODO: Adapt to non-blocking version
 void connect_mqtt()
 {
+  digitalWrite(LED_BUILTIN, LOW);
+
   // Wait for connection
   while (!client.connected())
   {
     const char *clientId = "ESP32Client-1";
-
     // First check WiFi connection
     if (WiFi.status() != WL_CONNECTED)
     {
@@ -82,16 +184,15 @@ void connect_mqtt()
       setup_wifi();
     }
 
-    Serial.print("Attempting MQTT connection...");
+    Serial.println("Attempting MQTT connection...");
 
     // Attempt connection
     if (client.connect(clientId))
     {
       Serial.println("Connected to MQTT server!");
-      // Publish a message to the topic "/outTopic"
-      Serial.println("publish");
+      digitalWrite(LED_BUILTIN, HIGH); // Turn on the LED
 
-      client.publish("esp32/sensor1", "Sensor 1 connecté!");
+      client.publish("esp32/photoresistance", "Connected photoresistance!");
       client.subscribe("rpi/broadcast");
     }
     else
@@ -132,11 +233,17 @@ void callback(char *topic, byte *payload, unsigned int length)
 // Run setup once
 void setup()
 {
+  // Initialize the serial port
   Serial.begin(9600);
+  // Initialize pins
+  pinMode(LED_BUILTIN, OUTPUT);
+  // Initialize WiFi and MQTT
   setup_wifi();
-  Serial.println("Connecting to MQTT broker...");
   client.setServer(mqtt_broker, MQTT_PORT);
   client.setCallback(callback);
+  // Initialize tasks
+  setup_Photo(&Photoresistance, TIMER0, 500000);
+  // setup_Publish(&Publish, TIMER1, 2000000, "/esp32/photoresistance");
 }
 
 void loop()
@@ -147,17 +254,18 @@ void loop()
     connect_mqtt();
   }
   client.loop();
+  loop_Photo(&Photoresistance);
 
   // Publish a message every 2 seconds
-  unsigned long now = millis();
-  if (now - lastPublishTime > MSG_INTERVAL)
-  {
-    lastPublishTime = now;
+  // unsigned long now = millis();
+  // if (now - lastPublishTime > MSG_INTERVAL)
+  // {
+  //   lastPublishTime = now;
 
-    ++value;
-    snprintf(msg, MSG_BUFFER_SIZE, "salut ma grosse poule #%ld", value);
-    Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish("/esp32/sensor1", msg);
-  }
+  //   ++value;
+  //   snprintf(msg, MSG_BUFFER_SIZE, "Photoresistance value: %ld", Photoresistance.val);
+  //   Serial.print("Message published: ");
+  //   Serial.println(msg);
+  //   client.publish("/esp32/photoresistance", msg);
+  // }
 }
